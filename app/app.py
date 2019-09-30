7b1247b196f1d04432f99d42f1540dddea9aca35
enixdark <cqshinn92@gmail.com>
2019-09-30 14:50:15 +0700

    fix: change config file

--- app/app.py
+++ app/app.py
@@ -75,5 +75,5 @@ def delete_item(item_id):
 
 if __name__ == "__main__":
     Schema()
-    app.run(debug=True, port=5000)
+    app.run('0.0.0.0', debug=True, port=5000)
     #app.run(debug=True, port=8888)
